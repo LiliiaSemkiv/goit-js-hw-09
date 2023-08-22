8ff0c929a722a1a9304a64dc823efe8e7b0511d3
LiliiaSemkiv <127901106+LiliiaSemkiv@users.noreply.github.com>
2023-08-22 20:56:14 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]')
+// console.log(buttonStart)
+// console.log(buttonStop)
+let interval = null;
+
+buttonStart.addEventListener("click", onStart);
+
+function onStart(){
+    if(!interval){
+        interval = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor()
+        }, 1000)
+        buttonStart.disabled = true;
+    }
+}
+
+buttonStop.addEventListener('click', onStop);
+
+function onStop(){
+    clearInterval(interval);
+    interval = null;
+    buttonStart.disabled = false;
+}
